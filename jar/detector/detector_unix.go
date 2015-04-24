3ebd3f4f5c58b7685bc9fbbbf68b1d73b7931604
Soli <soli@cbug.org>
2015-04-24 16:11:03 +0800

    implement default metrics;

--- jar/detector/detector_unix.go
+++ jar/detector/detector_unix.go
@@ -3,9 +3,13 @@
 package detector
 
 import (
+	"fmt"
+	"strconv"
 	"strings"
 	"syscall"
 	"unsafe"
+
+	"github.com/solicomo/host-stat-go"
 )
 
 func int8ToString(bs []int8) string {
@@ -45,5 +49,178 @@ func (Detector) OSVer(params ...string) (result string, err error) {
 			result += domName + " "
 		}
 	}
+	return
+}
+
+func (Detector) Uptime() (result string, err error) {
+
+	if upt, err := host_stat.GetUptimeStat(); err == nil {
+		result = fmt.Sprintf("%v", uint64(upt))
+	}
+
+	return
+}
+
+func (Detector) Load() (result string, err error) {
+
+	if load, err := host_stat.GetLoadStat(); err == nil {
+		result = fmt.Sprintf("%v, %v, %v", load.LoadNow, load.LoadPre, load.LoadFar)
+	}
+
+	return
+}
+
+func (Detector) CPUName() (result string, err error) {
+
+	if ci, err := host_stat.GetCPUInfo(); err == nil {
+		result = ci.ModelName
+	}
+
+	return
+}
+
+func (Detector) CPUCore() (result string, err error) {
+
+	if ci, err := host_stat.GetCPUInfo(); err == nil {
+		result = fmt.Sprintf("%v", ci.CoreCount)
+	}
+
+	return
+}
+
+func (Detector) CPURate() (result string, err error) {
+
+	if cs, err := host_stat.GetCPUStat(); err == nil {
+		result = fmt.Sprintf("%v", cs.UserRate)
+	}
+
+	return
+}
+
+func (Detector) MemSize() (result string, err error) {
+
+	if ms, err := host_stat.GetMemStat(); err == nil {
+		result = fmt.Sprintf("%v", ms.MemTotal)
+	}
+
+	return
+}
+
+func (Detector) MemRate() (result string, err error) {
+
+	if ms, err := host_stat.GetMemStat(); err == nil {
+		result = fmt.Sprintf("%v", ms.MemRate)
+	}
+
+	return
+}
+
+func (Detector) SwapRate() (result string, err error) {
+
+	if ms, err := host_stat.GetMemStat(); err == nil {
+		result = fmt.Sprintf("%v", ms.SwapRate)
+	}
+
+	return
+}
+
+func (Detector) DiskSize() (result string, err error) {
+
+	if ds, err := host_stat.GetDiskStat(); err == nil {
+
+		disk_total := uint64(0)
+
+		for _, v := range disk_stat {
+			disk_total += v.Total
+		}
+
+		result = fmt.Sprintf("%v", disk_total)
+	}
+
+	return
+}
+
+func (Detector) DiskRate() (result string, err error) {
+
+	if ds, err := host_stat.GetDiskStat(); err == nil {
+
+		disk_total := uint64(0)
+		disk_used := uint64(0)
+
+		for _, v := range disk_stat {
+			disk_total += v.Total
+			disk_used += v.Used
+		}
+
+		result = fmt.Sprintf("%v", Round(float64(disk_used)/float64(disk_total), 2))
+	}
+
+	return
+}
+
+func (Detector) DiskRead() (result string, err error) {
+
+	if is, err := host_stat.GetIOStat(); err == nil {
+
+		disk_read := uint64(0)
+
+		for _, v := range is {
+			disk_read += v.ReadBytes / 1024
+		}
+
+		result = fmt.Sprintf("%v", disk_read)
+	}
+
+	return
+}
+
+func (Detector) DiskWrite() (result string, err error) {
+	if is, err := host_stat.GetIOStat(); err == nil {
+
+		disk_write := uint64(0)
+
+		for _, v := range is {
+			disk_write += v.WriteBytes / 1024
+		}
+
+		result = fmt.Sprintf("%v", disk_write)
+	}
+
+	return
+}
+
+func (Detector) NetRead() (result string, err error) {
+
+	if ns, err := host_stat.GetNetStat(); err == nil {
+
+		net_read := uint64(0)
+
+		for _, v := range net_stat {
+			if v.Device != "lo" {
+				net_read += v.RXBytes / 1024
+			}
+		}
+
+		result = fmt.Sprintf("%v", net_read)
+	}
+
+	return
+}
+
+func (Detector) NetWrite() (result string, err error) {
+
+	if ns, err := host_stat.GetNetStat(); err == nil {
+
+		net_write := uint64(0)
+
+		for _, v := range net_stat {
+			if v.Device != "lo" {
+				net_write += v.TXBytes / 1024
+			}
+		}
+
+		result = fmt.Sprintf("%v", net_write)
+	}
+
 	return
 }
